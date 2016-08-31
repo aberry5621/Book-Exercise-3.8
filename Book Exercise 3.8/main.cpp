7dff71f59adc3449fd923899e5df98099134988b
alexberry721 <alexberry721@gmail.com>
2016-08-30 22:41:37 -0700

    Book listing 2.12 - change counting

--- Book Exercise 3.8/main.cpp
+++ Book Exercise 3.8/main.cpp
@@ -13,7 +13,39 @@ using namespace std;
 
 int main() {
 
-    // Code Here
+    // Receive the amount
+    cout << "Enter an amount in double, for example 11.56: ";
+    double amount;
+    cin >> amount;
+    
+    int remaining_amount = static_cast<int>(amount * 100);
+    
+    // get single dollars
+    int number_of_one_dollars = remaining_amount / 100;
+    remaining_amount = remaining_amount % 100;
+    
+    // find the number of quarters in the remaining amount
+    int number_of_quarters = remaining_amount / 25;
+    remaining_amount = remaining_amount % 25;
+    
+    // find the number of dimes in the remaining amount
+    int number_of_dimes = remaining_amount / 10;
+    remaining_amount = remaining_amount % 10;
+    
+    // find the number of nickels in the remaining amount
+    int number_of_nickels = remaining_amount / 5;
+    remaining_amount = remaining_amount % 5;
+    
+    // find the number of pennies in the remaining amount
+    int number_of_pennies = remaining_amount;
+    
+    // Display the results
+    cout << "Your amount " << amount << " consists of " << endl <<
+    "   " << number_of_one_dollars << " dollars" << endl <<
+    "   " << number_of_quarters << " quarters" << endl <<
+    "   " << number_of_dimes << " dimes" << endl <<
+    "   " << number_of_nickels << " nickels" << endl <<
+    "   " << number_of_pennies << " penis" << endl;
     
     return 0;
 }
