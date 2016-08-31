2e5e5a92bc966ab60b976d869fba5806f564ba97
alexberry721 <alexberry721@gmail.com>
2016-08-30 22:28:47 -0700

    Init commit

--- Book Exercise 3.8/main.cpp
+++ Book Exercise 3.8/main.cpp
@@ -5,11 +5,15 @@
 //  Created by ax on 8/30/16.
 //  Copyright © 2016 COMP130. All rights reserved.
 //
+//  Book exercise 3.8 - Financial Application: Monetary Units
+//
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+
+    // Code Here
+    
     return 0;
 }
